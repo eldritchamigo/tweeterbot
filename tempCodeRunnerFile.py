50e56ef0fcc505e97b7bffff988aea0eec111899
magic-f <149428610+eldritchamigo@users.noreply.github.com>
2024-10-13 20:56:22 -0700

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,9 @@
 import tweepy
 
-api_key = "eOlHfSX20URjTE8j444KIivSi"
-api_secret = "MOParXv44hvMo6GpS6hstIcMPfiioRBxX883LurdGUPUKoS8Dm"
-access_token = "1843448968899895296-aLPZVHu2S9mp2Sw6EgOgQSRRCREr99"
-access_token_secret = "v0qtWkRQN1mEZb6EopHQ8cLjWca4vLa6LN5q2tuwfr80V"
+api_key = "[REDACTED]"
+api_secret = "[REDACTED]"
+access_token = "[REDACTED]"
+access_token_secret = "[REDACTED]"
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
