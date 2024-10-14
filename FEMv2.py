9403af892d1526e810c6d488fe73ffdaf283b9c0
magic-f <149428610+eldritchamigo@users.noreply.github.com>
2024-10-13 21:25:00 -0700

    Update and rename FEMv1.py to FEMv2.py

--- FEMv2.py
+++ FEMv2.py
@@ -8,6 +8,7 @@ api_secret = "[REDACTED]"
 access_token = "[REDACTED]"
 access_token_secret = "[REDACTED]"
 
+#Twitter Authenticate
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 
